7638d36925788884d0df8cf39c5a00fd4a86a8c6
Tania Moses <taniamoses@Tanias-MacBook-Air.local>
2026-08-19 17:27:12 +0200

    feat(token): add transfer functionality

--- src/token_assets/src/components/App.jsx
+++ src/token_assets/src/components/App.jsx
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { AuthClient } from "@dfinity/auth-client";
+
 import Header from "./Header";
 import Faucet from "./Faucet";
 import Balance from "./Balance";
 import Transfer from "./Transfer";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  async function handleLogin() {
+    const authClient = await AuthClient.create();
+
+    await authClient.login({
+      identityProvider:
+        "http://localhost:8000/?canisterId=sbzkb-zqaaa-aaaaa-aaaiq-cai",
+      onSuccess: () => {
+        setIsAuthenticated(true);
+      },
+    });
+  }
 
   return (
     <div id="screen">
       <Header />
-      <Faucet />
-      <Balance />
-      <Transfer />
+
+      {!isAuthenticated ? (
+        <div className="window white">
+          <h2>Internet Identity</h2>
+
+          <p>Login to use your DANG tokens.</p>
+
+          <button onClick={handleLogin}>
+            Login with Internet Identity
+          </button>
+        </div>
+      ) : (
+        <>
+          <Faucet />
+          <Balance />
+          <Transfer />
+        </>
+      )}
     </div>
   );
 }
